9d6c3f23e1d5fd28497b9d4c41ae2186131328d8
Lyndon R / lyndon025 <lyndongaming@gmail.com>
2025-08-11 21:48:52 +0800

    test decapcms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
-
 export default defineConfig({
-  site: 'https://playtested.net',
+  site: "https://playtested.net",
   integrations: [sitemap()],
-  adapter: cloudflare(),     // ✅ Cloudflare Workers (not Pages)
-  output: 'server',          // ✅ Needed for Workers support
+  adapter: cloudflare({ mode: "directory" }), // ✅ Cloudflare Workers (not Pages)
+  output: "server", // ✅ Needed for Workers support
   vite: {
     plugins: [tailwindcss()],
   },
-    image: {
-    service: { entrypoint: "astro/assets/services/squoosh" }
+  image: {
+    service: { entrypoint: "astro/assets/services/squoosh" },
   },
 });
