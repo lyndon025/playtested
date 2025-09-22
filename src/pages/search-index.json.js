8420e07111430021fe34aa35d6be4e286bcb7271
Lyndon R / lyndon025 <lyndongaming@gmail.com>
2025-09-22 22:59:12 +0800

    updated search function to be more dynamic

---
+++ src/pages/search-index.json.js
@@ -0,0 +1,23 @@
+import { getCollection } from "astro:content";
+
+export async function GET() {
+  const articles = await getCollection("article");
+  const submissions = await getCollection("submissions");
+  const allPosts = [...articles, ...submissions];
+
+  const searchIndex = allPosts.map((post) => ({
+    id: `${post.collection}-${post.slug}`,
+    title: post.data.title,
+    description: post.data.description ?? "",
+    tags: post.data.tags?.join(", ") ?? "",
+    body: post.body,
+    url: `/${post.collection}/${post.slug}`,
+    pubDate: post.data.pubDate,
+  }));
+
+  return new Response(JSON.stringify(searchIndex), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
