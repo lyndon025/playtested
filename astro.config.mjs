43ca5173c95a5ac84d31b50b244f022a5137f380
Lyndon R / lyndon025 <lyndonr025@gmail.com>
2026-01-08 18:37:34 +0800

    Remove explicit admin redirect to fix loop

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ export default defineConfig({
   site: "https://playtested.net",
   output: "static", // Static build to /dist
   trailingSlash: "always", // or "never" - choose one and be consistent
-  redirects: {
-    "/admin": "/admin/"
-  },
   integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
