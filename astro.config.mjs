61e45e638c58ad6ed8653f62539208d18fcfc91c
Lyndon R / lyndon025 <lyndonr025@gmail.com>
2026-01-08 18:28:37 +0800

    Fix carousel, FOUC, layout width, and add exp deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ export default defineConfig({
   site: "https://playtested.net",
   output: "static", // Static build to /dist
   trailingSlash: "always", // or "never" - choose one and be consistent
+  redirects: {
+    "/admin": "/admin/"
+  },
   integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
